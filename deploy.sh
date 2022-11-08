cc7852d5eb25ca29574ee641f05df823006253fb
Jason Racey <jasonracey@gmail.com>
2022-11-07 21:48:21 -0800

    Update deployment to remove old version and build with release flag

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# deploy app
-echo "Deploying App..."
+echo "Removing App..."
 rm -Rf /Applications/Media\ Downloader.app
+
+echo "Building..."
+rm -Rf ./MediaDownloaderUI/bin/Release/*
+msbuild MediaDownloader.sln /property:Configuration=Release
+
+echo "Deploying App..."
 cp -Rf ./MediaDownloaderUI/bin/Release/Media\ Downloader.app /Applications/Media\ Downloader.app
 
-echo "Done!"
+echo "Done. See output for results."
